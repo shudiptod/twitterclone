e9b1fb9b74c170a2784687f0c9cb0c373a11fc4a
shudipto <shudipta15-11424@diu.edu.bd>
2020-10-29 01:02:59 +0600

    added profile page url

--- twitterclone/urls.py
+++ twitterclone/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='user/login.html'),name="login"),
     path('register/',user_views.register,name="register"),
     path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'),name="logout"),
+    path('profile/',user_views.profile,name="profile")
 ]
